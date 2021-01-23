6d33b17e47263450eb7277c62b707e8a0d066896
viniljose <vinilpj@gmail.com>
2021-01-23 18:54:21 +0530

    Initial set of files

---
+++ src/main/java/com/vinil/reactive/EventListenerSample/EventListenerSampleApplication.java
@@ -0,0 +1,13 @@
+package com.vinil.reactive.EventListenerSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventListenerSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventListenerSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinil/reactive/EventListenerSample/EventListenerSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinil.reactive.EventListenerSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventListenerSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
