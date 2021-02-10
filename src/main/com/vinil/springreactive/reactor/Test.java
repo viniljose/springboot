10464c72f6995ce74cb65ee8e81b75d445dfcf09
viniljose <vinilpj@gmail.com>
2021-02-10 22:19:38 +0530

    CompletableFuture

--- src/main/com/vinil/springreactive/reactor/Test.java
+++ src/main/com/vinil/springreactive/reactor/Test.java
@@ -2,13 +2,39 @@ package com.vinil.springreactive.reactor;
 
 import reactor.core.publisher.Flux;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class Test {
     public static void main(String[] args) {
 
         //This generates Out of Memory Error.
-        Flux.range(1, 100)                                                  // (1)
+        /*Flux.range(1, 100)                                                  // (1)
                 .repeat()                                                        // (2)
                 .collectList()                                                   // (3)
-                .block();
+                .block();*/
+        //CompletableFuture Sample
+        Test tst = new Test();
+        try {
+            Future<String> completableFuture = tst.calculateAsync();
+            String result = completableFuture.get();
+            System.out.println(result);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Future<String> calculateAsync() throws InterruptedException {
+        CompletableFuture<String> completableFuture = new CompletableFuture<>();
+
+        Executors.newCachedThreadPool().submit(() -> {
+            Thread.sleep(500);
+            completableFuture.complete("Hello");
+            return null;
+        });
+
+        return completableFuture;
     }
 }
